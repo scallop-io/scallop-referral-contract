b843623b8f83c19127c69e80d905deae85511d05
icefox <zengxinhai001@163.com>
2024-05-22 22:05:25 +0800

    Deploy for production

--- scripts/addReferralTiers.ts
+++ scripts/addReferralTiers.ts
@@ -6,11 +6,11 @@ addReferralTiers().then(console.log);
 async function addReferralTiers() {
   const tiers = [
     { veSCA: 0, referralShare: 10, borrow_fee_discount: 10 },
-    { veSCA: 100, referralShare: 15, borrow_fee_discount: 10 },
-    { veSCA: 1000, referralShare: 20, borrow_fee_discount: 10 },
-    { veSCA: 10000, referralShare: 30, borrow_fee_discount: 10 },
-    { veSCA: 100000, referralShare: 40, borrow_fee_discount: 10 },
-    { veSCA: 1000000, referralShare: 50, borrow_fee_discount: 10 },
+    { veSCA: 100e9, referralShare: 15, borrow_fee_discount: 10 },
+    { veSCA: 1000e9, referralShare: 20, borrow_fee_discount: 10 },
+    { veSCA: 10000e9, referralShare: 30, borrow_fee_discount: 10 },
+    { veSCA: 100000e9, referralShare: 40, borrow_fee_discount: 10 },
+    { veSCA: 1000000e9, referralShare: 50, borrow_fee_discount: 10 },
   ];
 
   const tx = new SuiTxBlock();
--- scripts/txBuilder.ts
+++ scripts/txBuilder.ts
@@ -1,11 +1,18 @@
 import { SuiTxBlock } from '@scallop-io/sui-kit';
-import Contract from '../publish-result.test.json';
+import Contract from '../publish-result.production.json';
 
 export class ScallopReferralTxBuilder {
   static add_referral_tier(tx: SuiTxBlock, veSCA: number, referralShare: number, borrow_fee_discount: number) {
     tx.moveCall(
       `${Contract.packageId}::admin::add_referral_tier`,
       [Contract.adminCap, Contract.referralTiers, veSCA, referralShare, borrow_fee_discount],
+    )
+  }
+
+  static remove_referral_tier(tx: SuiTxBlock, veSCA: number) {
+    tx.moveCall(
+      `${Contract.packageId}::admin::remove_referral_tier`,
+      [Contract.adminCap, Contract.referralTiers, veSCA],
     )
   }
 }
