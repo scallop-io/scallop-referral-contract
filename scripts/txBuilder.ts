3c0dd026981901b9fa1b0ce103f383ef3ad7d2f7
nathanramli <hezkyanatanael84@gmail.com>
2026-05-26 18:12:54 +0700

    chore: update referral script

--- scripts/txBuilder.ts
+++ scripts/txBuilder.ts
@@ -5,21 +5,21 @@ export class ScallopReferralTxBuilder {
   static addReferralTierV2(tx: SuiTxBlock, veSCA: number, referralShare: number, borrow_fee_discount: number) {
     tx.moveCall(
       `${Contract.packageId}::admin::add_referral_tier_v2`,
-      [Contract.adminCapV2, Contract.referralTiers, veSCA, referralShare, borrow_fee_discount],
+      [Contract.adminCapV2, Contract.referralTiers, tx.pure.u64(veSCA), tx.pure.u64(referralShare), tx.pure.u64(borrow_fee_discount)],
     )
   }
 
   static removeReferralTierV2(tx: SuiTxBlock, veSCA: number) {
     tx.moveCall(
       `${Contract.packageId}::admin::remove_referral_tier_v2`,
-      [Contract.adminCapV2, Contract.referralTiers, veSCA],
+      [Contract.adminCapV2, Contract.referralTiers, tx.pure.u64(veSCA)],
     )
   }
 
   static setContractVersionV2(tx: SuiTxBlock, newVersion: number) {
     tx.moveCall(
       `${Contract.packageId}::admin::set_contract_version_v2`,
-      [Contract.adminCapV2, Contract.versionObject,newVersion]
+      [Contract.adminCapV2, Contract.versionObject, tx.pure.u64(newVersion)]
     )
   }
 
--- scripts/updateReferralTiers.ts
+++ scripts/updateReferralTiers.ts
@@ -1,25 +1,26 @@
 import { SuiTxBlock } from '@scallop-io/sui-kit';
 import { ScallopReferralTxBuilder } from './txBuilder';
 import { adminSuiKit } from './suiElements';
+import { buildMultiSigTx, MULTI_SIG_ADDRESS } from './multiSig';
 
 updateReferralTiers().then(console.log);
 async function updateReferralTiers() {
   const oldTiers = [
-    { veSCA: 0, referralShare: 10, borrow_fee_discount: 10 },
-    { veSCA: 100e9, referralShare: 15, borrow_fee_discount: 12 },
-    { veSCA: 1000e9, referralShare: 20, borrow_fee_discount: 14 },
-    { veSCA: 10000e9, referralShare: 25, borrow_fee_discount: 16 },
-    { veSCA: 100000e9, referralShare: 30, borrow_fee_discount: 18 },
-    { veSCA: 1000000e9, referralShare: 40, borrow_fee_discount: 20 },
-  ];
-
-  const newTiers = [
     { veSCA: 0, referralShare: 5, borrow_fee_discount: 5 },
     { veSCA: 100e9, referralShare: 6, borrow_fee_discount: 7 },
     { veSCA: 1000e9, referralShare: 9, borrow_fee_discount: 12 },
     { veSCA: 10000e9, referralShare: 18, borrow_fee_discount: 25 },
     { veSCA: 100000e9, referralShare: 32, borrow_fee_discount: 50 },
     { veSCA: 1000000e9, referralShare: 40, borrow_fee_discount: 60 },
+  ];
+
+  const newTiers = [
+    { veSCA: 0, referralShare: 0, borrow_fee_discount: 0 },
+    { veSCA: 1_000e9, referralShare: 5, borrow_fee_discount: 5 },
+    { veSCA: 50_000e9, referralShare: 15, borrow_fee_discount: 15 },
+    { veSCA: 100_000e9, referralShare: 25, borrow_fee_discount: 20 },
+    { veSCA: 500_000e9, referralShare: 35, borrow_fee_discount: 35 },
+    { veSCA: 1_000_000e9, referralShare: 50, borrow_fee_discount: 50 },
   ];
 
   const tx = new SuiTxBlock();
@@ -30,5 +31,11 @@ async function updateReferralTiers() {
     ScallopReferralTxBuilder.addReferralTierV2(tx, tier.veSCA, tier.referralShare, tier.borrow_fee_discount);
   }
 
-  return adminSuiKit.signAndSendTxn(tx);
+  tx.setSender(MULTI_SIG_ADDRESS);
+  const txBytes = await buildMultiSigTx(tx);
+  const resp = await adminSuiKit.suiInteractor.currentClient.dryRunTransactionBlock({
+    transactionBlock: txBytes
+  });
+  console.log('Dry run response:', resp);
+  return txBytes;
 }
